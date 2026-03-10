55100dbfd6c62b385ae453f66e65db1451586390
hafey-lx <zhu460017149@163.com>
2026-03-11 02:50:27 +0800

    Configure site for pumpkinshare.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -39,7 +39,7 @@ import PlayformInline from "@playform/inline";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://preview.astro.kaitaku.xyz",
+  site: "https://pumpkinshare.github.io",
   trailingSlash: "always",
   build: {
     format: "directory",
